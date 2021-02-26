4df80c2b9f2b9c38437227fc13c1ff697c955329
yuno74 <65702879+yuno74@users.noreply.github.com>
2021-02-26 17:08:54 +0800

    Add files via upload

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,23 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start1'
+        self.MirrorCommand = 'mir1'
+        self.UnzipMirrorCommand = 'unzip1'
+        self.TarMirrorCommand = 'tarmir1'
+        self.CancelMirror = 'cancel1'
+        self.CancelAllCommand = 'cancelall1'
+        self.ListCommand = 'list1'
+        self.StatusCommand = 'status1'
+        self.AuthorizeCommand = 'auth1'
+        self.UnAuthorizeCommand = 'unauth1'
+        self.PingCommand = 'ping1'
+        self.RestartCommand = 'restart1'
+        self.StatsCommand = 'stats1'
+        self.HelpCommand = 'help1'
+        self.LogCommand = 'log1'
+        self.CloneCommand = "clone1"
+        self.WatchCommand = 'yt1'
+        self.TarWatchCommand = 'taryt1'
+        self.deleteCommand = 'del1'
+
+BotCommands = _BotCommands()
